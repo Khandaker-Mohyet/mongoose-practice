1d1de84c9b3b9d9e9f32bcc7eb43c2391bf8fb82
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-18 05:06:08 +0600

    get oprationg

--- src/app.ts
+++ src/app.ts
@@ -3,35 +3,63 @@ import { model, Schema } from 'mongoose';
 
 const app:Application = express()
 
+app.use(express.json())
+
 const noteSchema = new Schema({
     title: {type: String, require: true, trim: true},
     contact: {type: String, default:""},
     category:{
         type: String,
         enum:["person", "work", "study", "other"],
-        default: "personal"
+        default: "person"
     },
     pinned:{
         type:Boolean,
         default: false
+    },
+    tags:{
+        label:{type:String, require: true},
+        color:{type: String, default: 'Green'}
     }
 })
 
 const Note = model("Note", noteSchema)
 
-app.post('/create-note', async (req:Request, res:Response)=>{
-    const myNote = new Note({
-        title: "Learning Mongoose",
-        contact: "I am learning MOngoose",
-        price:100
-    })
+app.post('/notes/create-note', async (req:Request, res:Response)=>{
+    
+    const body = req.body;
 
-    await myNote.save()
+    // approach 1- of creating a data
+    // const myNote = new Note({
+    //     title: "Learning Express",
+    //     tags:{
+    //         label: "database"
+    //     }
+    // })
+
+    // await myNote.save()
+
+    // approach-2
+
+    const note = await Note.create(body)
 
     res.status(201).json({
         success: true,
         message: "Note created successfully",
-        note: myNote
+        note
+    })
+})
+
+
+
+app.get('/notes', async (req:Request, res:Response)=>{
+
+    const notes = await Note.find()
+
+    res.status(201).json({
+        success: true,
+        message: "Note created successfully",
+        notes
     })
 })
 
