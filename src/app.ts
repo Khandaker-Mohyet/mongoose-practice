ab6eb3ffc310365868c546a4dac298fc94810472
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-18 03:13:28 +0600

    connect to mongoose

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,7 @@ import express,{Application, Request, Response} from 'express';
 const app:Application = express()
 
 export default app;
+
+app.get('/', (req:Request, res:Response)=>{
+    res.send("welcome to Khendaker Mohyet work station")
+})
--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,10 @@ const PORT = 5000;
 
 async function main() {
     try {
+        await mongoose.connect("mongodb+srv://mongodb:nrmXqzUhIs0qRMdq@cluster0.47f5u.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0")
         server = app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-  })
+            console.log(`Example app listening on port ${PORT}`)
+        })
     } catch (error) {
         console.log(error)
     }
