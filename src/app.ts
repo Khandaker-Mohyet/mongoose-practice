53f54a0ff9fb752585d11087bc3234e594935424
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-18 03:27:24 +0600

    frist schema

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express,{Application, Request, Response} from 'express';
+import { model, Schema } from 'mongoose';
 
 const app:Application = express()
 
-export default app;
+const noteSchema = new Schema({
+    title: String,
+    contact: String,
+    price: Number
+})
+
+const Note = model("Note", noteSchema)
+
+
+
 
 app.get('/', (req:Request, res:Response)=>{
     res.send("welcome to Khendaker Mohyet work station")
 })
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ const PORT = 5000;
 async function main() {
     try {
         await mongoose.connect("mongodb+srv://mongodb:nrmXqzUhIs0qRMdq@cluster0.47f5u.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0")
+        console.log("Connected ot MongoDB using Mongoose")
         server = app.listen(PORT, () => {
             console.log(`Example app listening on port ${PORT}`)
         })
