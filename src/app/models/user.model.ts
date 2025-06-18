3a0d92f665b4689258cb8b4e1e9062cb2065e86e
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-19 02:57:45 +0600

    schema validation

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -4,35 +4,48 @@ import { IUser } from "../interfaces/user.interface";
 
 const usersSchema = new Schema<IUser>(
     {
-firstName:{
-    type: String,
-    required: true,
-    trim: true
-},
-lastName:{
-    type: String,
-    required: true,
-    trim: true
-},
-email:{
-    type: String,
-    required: true,
-    trim: true
-},
-password:{
-    type: String,
-    required: true
-},
-role:{
-    type: String,
-    enum:['user', 'admin'],
-    default: 'user'
-}
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength:5,
+            maxlength:10
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            lowercase: true,
+            unique: [true, 'Email common hoya gasa'],
+            required: true,
+            trim: true
+        },
+        age: {
+            type: Number,
+            required: true,
+            min:18,
+            max:60
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            enum: {
+                values:['user', 'admin','superadmin'],
+                message:"Role is not valid. got {value} role"
+            },
+            default: 'user'
+        }
 
-},
-{
-    timestamps: true
-}
+    },
+    {
+        timestamps: true
+    }
 )
 
 export const User = model("user", usersSchema)
